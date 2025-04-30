10ba6ea4c32ef3eb8582cf17e1138fe1bb94ed97
Dmitrii <73591672+bushuevda@users.noreply.github.com>
2025-04-30 12:41:03 +0500

    Add files via upload

---
+++ quadratic_equation/quadratic_equation.hpp
@@ -0,0 +1,35 @@
+#ifndef QUADRATIC_EQUATION_HPP
+#define QUADRATIC_EQUATION_HPP 1
+
+#include <iostream>
+#include <cmath>
+#include <cassert>
+
+
+class QuadraticEquation{
+    private:
+        double a_;
+        double b_;
+        double c_;
+        double x1_;
+        double x2_;
+        int root_count_;
+
+    public:
+        QuadraticEquation(){}
+        double get_x1();
+        double get_x2();
+        int get_root_count();
+
+        //set a, b and c
+        QuadraticEquation& set(double a, double b, double c);
+
+        //calculate roots
+        QuadraticEquation& calculate();
+
+        //result calculate
+        void show();
+
+};
+
+#endif
